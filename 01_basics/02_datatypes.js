2a2d7d0693e5b9c4aa1be446112fe5eb433cac2f
Devesh <143078789+devesh816867@users.noreply.github.com>
2025-05-13 15:11:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version.
+// alert("hello")  - we are using node js not browser.
+let age = 14
+//number - 2 to the power 53
+//bigint
+// string - ""
+// boolean - true/false
+//null - standalone value
+//undefined 
+//symbol - unique
+//object
+
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
