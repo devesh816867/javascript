fd48520c704168c995a1e5d974893333c66915f6
Devesh <143078789+devesh816867@users.noreply.github.com>
2025-05-13 09:09:29 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=12345;
+let accountEmail= "devesh287@gmail.com"
+var accountPassword=12345
+accountCity="chandigarh"
+accountEmail="adbc@gmail.com"
+accountPassword="245"
+accountCity="bihar"
+console.table([accountEmail,accountPassword,accountCity])
