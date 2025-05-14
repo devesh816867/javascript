3609e7477b1d1cac3c17cdcec67d9b11bc1ec267
Devesh <143078789+devesh816867@users.noreply.github.com>
2025-05-14 14:54:59 +0000

    done

--- 01_basics/03_dconversion.js
+++ 01_basics/03_dconversion.js
@@ -19,7 +19,15 @@ console.log( valueInNumber )*/
 //""--> false
 //"hiee"--> true
 
-let someNumber =66
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//let someNumber =66
+//let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+let str1="Hello"
+let str2 = " Devesh"
+let str3= str1+str2
+console.log(str3)
+
+console.log(+true)
+console.log(+"")
