7dfc3a85d4a1b67030502f9a309b4d869a277f63
Devesh <143078789+devesh816867@users.noreply.github.com>
2025-05-14 07:25:24 +0000

    done

---
+++ 01_basics/03_dconversion.js
@@ -0,0 +1,25 @@
+/*let score = "33abc"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof  valueInNumber )
+console.log( valueInNumber )*/
+
+//33abc --> NAN
+// TRUE --> 1
+// false--> 0
+
+//let isLogin = 1;
+//let booleanisLogin= Boolean(isLogin)
+//console.log(booleanisLogin)
+
+// 1--> true
+//0--> false
+//""--> false
+//"hiee"--> true
+
+let someNumber =66
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
